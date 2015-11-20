1c6d701dcdf1dc910fe7896a25b1f1ae0ce1945a
Grega Vrbančič <grega.vrbancic@gmail.com>
2015-11-20 15:28:23 +0100

    edit production mongodb config

--- src/main/java/si/um/feri/obu/ProdMongoDBConfig.java
+++ src/main/java/si/um/feri/obu/ProdMongoDBConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
@@ -18,7 +19,14 @@ public class ProdMongoDBConfig {
 
     @Bean
     public MongoDbFactory mongoDbFactory() throws Exception {
-        return new SimpleMongoDbFactory(new MongoClient(System.getenv("OPENSHIFT_MONGODB_DB_URL")), dbName);
+        String openshiftMongoDbHost = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
+        int openshiftMongoDbPort = Integer.parseInt(System.getenv("OPENSHIFT_MONGODB_DB_PORT"));
+        String username = System.getenv("OPENSHIFT_MONGODB_DB_USERNAME");
+        String password = System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD");
+        UserCredentials userCredentials = new UserCredentials(username,password);
+        String databaseName = System.getenv("OPENSHIFT_APP_NAME");
+        MongoDbFactory mongoDbFactory = new SimpleMongoDbFactory(new MongoClient(openshiftMongoDbHost, openshiftMongoDbPort), dbName, userCredentials);
+        return mongoDbFactory;
     }
 
     @Bean
