86ea42dfda61a941899339d3b0ba1758355fbdff
Grega Vrbančič <grega.vrbancic@gmail.com>
2015-11-24 15:24:16 +0100

    dokku config

--- src/test/java/si/um/feri/obu/ApplicationWSTests.java
+++ src/test/java/si/um/feri/obu/ApplicationWSTests.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.ClassUtils;
@@ -19,9 +20,10 @@ import static org.junit.Assert.assertNotNull;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ObuApplication.class)
 @WebIntegrationTest("server.port:0")
+@Profile("dev")
 public class ApplicationWSTests {
 
-    private static final String HOST = System.getenv("OPENSHIFT_DIY_IP");
+    private static final String HOST = "http://127.0.0.1:";
     private static final String WS = "/ws";
 
     private Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
