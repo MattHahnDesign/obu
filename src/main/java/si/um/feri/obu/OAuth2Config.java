c2f045fd3f1848a60b0b1c1a0fd155f585bed669
Grega Vrbančič <grega.vrbancic@gmail.com>
2016-01-14 19:56:12 +0100

    change properties

--- src/main/java/si/um/feri/obu/OAuth2Config.java
+++ src/main/java/si/um/feri/obu/OAuth2Config.java
@@ -15,6 +15,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -87,6 +88,12 @@ public class OAuth2Config {
             endpoints
                     .tokenStore(tokenStore())
                     .authenticationManager(authenticationManager);
+        }
+
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+            oauthServer
+                    .allowFormAuthenticationForClients();
         }
 
         @Override
--- src/main/java/si/um/feri/obu/api/RestControllerOBU.java
+++ src/main/java/si/um/feri/obu/api/RestControllerOBU.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpServerErrorException;
 import si.um.feri.obu.domain.model.CarCommandParameter;
 import si.um.feri.obu.domain.model.GetCarCommandStateListReponse;
 import si.um.feri.obu.domain.xjc.*;
